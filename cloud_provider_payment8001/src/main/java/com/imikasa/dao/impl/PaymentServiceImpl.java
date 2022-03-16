0bb737a56264cfebf57774822d64486cfa71e354
Mikasa <3127736010@qq.com>
2022-03-17 00:37:27 +0800

    spring cloud study in 2022 by mikasa

---
+++ cloud_provider_payment8001/src/main/java/com/imikasa/dao/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.imikasa.dao.impl;
+
+import com.imikasa.dao.PaymentDao;
+import com.imikasa.entities.Payment;
+import com.imikasa.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
